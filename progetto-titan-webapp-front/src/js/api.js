f1e85aaff78545169f2574fe6e697777f52d4743
Andrea Altomare <164928731+andrea-340@users.noreply.github.com>
2026-01-12 22:23:13 +0100

    Delete progetto-titan-webapp-front directory

--- progetto-titan-webapp-front/src/js/api.js
+++
@@ -1,9 +0,0 @@
-async function apiRegistrazione(datiDaInviare) {
-  const response = await fetch("http://localhost:8080/api/registrazione", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(datiDaInviare),
-  });
-  return await response.json();
-}
-export { apiRegistrazione };
